be2b9ad59a537c59c7b0c5727e65901a95bd0d36
goreleaser <goreleaser@users.noreply.github.com>
2026-09-04 02:23:05 +0300

    Brew formula update for tp version v1.0.0

--- Formula/tp.rb
+++ Formula/tp.rb
@@ -5,21 +5,21 @@
 class Tp < Formula
   desc "Spec-to-task lifecycle manager for AI coding agents"
   homepage "https://github.com/deligoez/tp"
-  version "0.37.0"
+  version "1.0.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/deligoez/tp/releases/download/v0.37.0/tp_0.37.0_macOS_x86_64.tar.gz"
-      sha256 "faef8e88a30f49126b6dea1ed997a47cc887e0d52730444d50b11fae5e62bbfe"
+      url "https://github.com/deligoez/tp/releases/download/v1.0.0/tp_1.0.0_macOS_x86_64.tar.gz"
+      sha256 "6fe60922c1dd3e49eb0c1f28487600fe29fa5669e131454d8017d7bfd329b056"
 
       define_method(:install) do
         bin.install "tp"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/deligoez/tp/releases/download/v0.37.0/tp_0.37.0_macOS_arm64.tar.gz"
-      sha256 "19b7d76a8ee1d3d935b6eb0305d2987df2e8a7c5d49ebd89605cae0642e024c7"
+      url "https://github.com/deligoez/tp/releases/download/v1.0.0/tp_1.0.0_macOS_arm64.tar.gz"
+      sha256 "8ed2a041475b26ba50a43cedf911269458910206e34866a7d378b45c3fffd705"
 
       define_method(:install) do
         bin.install "tp"
@@ -29,15 +29,15 @@ class Tp < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/deligoez/tp/releases/download/v0.37.0/tp_0.37.0_linux_x86_64.tar.gz"
-      sha256 "54f94d207e9a413f213a1b787f7fe33d1556026b4724321387ceeab540a0a49e"
+      url "https://github.com/deligoez/tp/releases/download/v1.0.0/tp_1.0.0_linux_x86_64.tar.gz"
+      sha256 "3b2565fc7e8d65c1b5d514dcab176b786250d2cb9a237cedf9d417e82b94900c"
       define_method(:install) do
         bin.install "tp"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/deligoez/tp/releases/download/v0.37.0/tp_0.37.0_linux_arm64.tar.gz"
-      sha256 "8b8a8d997ab0e4e6c48e04fa14e6b393c6bc029382d7e3d5699c89fae464dc37"
+      url "https://github.com/deligoez/tp/releases/download/v1.0.0/tp_1.0.0_linux_arm64.tar.gz"
+      sha256 "478cdd0f5871ee8a18b4a57c0cca0e639e04cfa3c12d35e7a1f9ecb912ce5db4"
       define_method(:install) do
         bin.install "tp"
       end
