aaa0e6e234507ab18e162193dd889c1e475edbf9
goreleaser <goreleaser@users.noreply.github.com>
2026-09-23 14:20:31 +0300

    Brew formula update for cr version v0.7.1

--- Formula/cr.rb
+++ Formula/cr.rb
@@ -5,21 +5,21 @@
 class Cr < Formula
   desc "Code review lifecycle manager for AI coding agents"
   homepage "https://github.com/deligoez/cr"
-  version "0.7.0"
+  version "0.7.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/deligoez/cr/releases/download/v0.7.0/cr_0.7.0_macOS_x86_64.tar.gz"
-      sha256 "492bb55e0599b07c22bacce0ed12d88041eeb23948b85258cdd1dded51567ae5"
+      url "https://github.com/deligoez/cr/releases/download/v0.7.1/cr_0.7.1_macOS_x86_64.tar.gz"
+      sha256 "7cc7887b1e629a11a363901c85631c831635f8be5f3aac4440f9bc11c8ba242b"
 
       define_method(:install) do
         bin.install "cr"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/deligoez/cr/releases/download/v0.7.0/cr_0.7.0_macOS_arm64.tar.gz"
-      sha256 "c83805b99f8d1585d209721cc4c9e95163a3a545a5b0031ee94bc55ec9993d0a"
+      url "https://github.com/deligoez/cr/releases/download/v0.7.1/cr_0.7.1_macOS_arm64.tar.gz"
+      sha256 "f1d1f4cff3fe640fe8dd49437c0466395d2dc2da766b575053d0c4779aa27428"
 
       define_method(:install) do
         bin.install "cr"
@@ -29,15 +29,15 @@ class Cr < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/deligoez/cr/releases/download/v0.7.0/cr_0.7.0_linux_x86_64.tar.gz"
-      sha256 "1e952660d51a8d73be9fcce9dcd6ebd71e47e973d96e9b332bf6c4a933049027"
+      url "https://github.com/deligoez/cr/releases/download/v0.7.1/cr_0.7.1_linux_x86_64.tar.gz"
+      sha256 "65e334e8cc2f656d66531fc70ec5a3129a4fe32946c64b937936296e4648b583"
       define_method(:install) do
         bin.install "cr"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/deligoez/cr/releases/download/v0.7.0/cr_0.7.0_linux_arm64.tar.gz"
-      sha256 "c190d4fb68955ef5421a9ba3a33b30fcf14dd87ced6c5716d9905b16fa2447d3"
+      url "https://github.com/deligoez/cr/releases/download/v0.7.1/cr_0.7.1_linux_arm64.tar.gz"
+      sha256 "fc84aad633d7c077744cb59a3e52012ce9ace1b4971c2efded1008d28eeb6e7c"
       define_method(:install) do
         bin.install "cr"
       end
